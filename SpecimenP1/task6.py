245989fb3d1fc24a55eb82e3bc5d9ca660187816
yiyang00 <125617010+yiyang00@users.noreply.github.com>
2023-03-03 10:41:32 +0800

    Update task6.py

--- SpecimenP1/task6.py
+++ SpecimenP1/task6.py
@@ -1,1 +1,20 @@
+# PCTC Round 1 Demo 6
 
+name1 = input()
+name2 = input()
+name3 = input()
+name4 = input()
+name5 = input()
+name_list = []
+name_list += name1, name2, name3, name4, name5
+
+if name_list[0] == "Ellie":
+    print("1st")
+elif name_list[1] == "Ellie":
+    print("2nd")
+elif name_list[2] == "Ellie":
+    print("3rd")
+elif name_list[3] == "Ellie":
+    print("4th")
+else:
+    print("5th")        
