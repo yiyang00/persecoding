53f6c8ba52c9c1a1905bc416fac220d05179598f
yiyang00 <125617010+yiyang00@users.noreply.github.com>
2023-03-03 11:26:08 +0800

    Update task9.py

--- SpecimenP1/task9.py
+++ SpecimenP1/task9.py
@@ -1,2 +1,36 @@
 # PCTC Round 1 Demo 9
 
+start = input()
+dance = ['<', '+', '&', '>']
+count = 0
+dance_sequence = ""
+
+if dance[0] == start:
+  dance_sequence += dance[0]
+  seq_count = 1
+  count += 1 
+elif dance[1] == start:
+  dance_sequence += dance[1]
+  seq_count = 2
+  count += 1
+elif dance[2] == start:
+  dance_sequence += dance[2]
+  seq_count = 3
+  count += 1
+else:
+  dance_sequence += dance[3]
+  seq_count = 4
+  count += 1
+
+while count < 8:
+  if seq_count != 4:
+    dance_sequence += dance[seq_count]
+    seq_count += 1
+    count += 1
+  else:
+    seq_count = 0 
+    dance_sequence += dance[seq_count]
+    seq_count += 1 
+    count += 1
+
+print(dance_sequence)
