7adb13aab2961810fee8447569543f2d0ae42d69
yiyang00 <125617010+yiyang00@users.noreply.github.com>
2023-03-06 12:18:07 +0800

    Update taskX10.py

--- SpecimenP1/taskX10.py
+++ SpecimenP1/taskX10.py
@@ -1,36 +1,27 @@
 # PCTC Round 1 Demo 10
 
 milkshake_char = input()
-I_count = 0
-M_count = 0
-C_count = 0
-W_count = 0
+I_requirement = 2
+M_requirement = 1
+C_requirement = 3
+W_requirement = 1
+missing = ""
+
 for i in range(len(milkshake_char)):
   if milkshake_char[i] == "I":
-    I_count += 1
+    I_requirement -= 1
   elif milkshake_char[i] == "M":
-    M_count += 1
+    M_requirement -= 1
   elif milkshake_char[i] == "C":
-    C_count += 1
+    C_requirement -= 1
   else:
-    W_count += 1
+    W_requirement -= 1
 
-missing = ""
-if I_count != 2:
-  more_I = 2 - I_count
-  while more_I != 0:
-    missing += "I"
-    more_I -= 1
-if M_count != 1:
-  more_M = 1
-  missing += "M"
-if C_count != 3:
-  more_C = 3 - C_count
-  while more_C != 0:
-    missing += "C"
-    more_C -= 1
-if W_count != 1:
-  more_W = 1
-  missing += "w"
-
-print(missing)
+if I_requirement != 0:
+  print("I")
+elif M_requirement != 0:
+  print("M")
+elif C_requirement != 0:
+  print("C")
+else:
+  print("W")
